351b9e286a48b01cdd1441d1cd4b3fa03c67c878
moons14 <moons@moons14.com>
2022-12-04 16:47:43 +0900

    feat:Color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,42 @@ import { AppProps } from 'next/app';
 import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
 export default function App(props: AppProps) {
+
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [
+      alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID || "" }),
+      publicProvider()
+    ]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: 'Address to Color',
+    chains
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+
   const { Component, pageProps } = props;
   const preferredColorScheme = useColorScheme();
   const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
@@ -20,7 +55,11 @@ export default function App(props: AppProps) {
             withNormalizeCSS
             theme={{ colorScheme }}
           >
-            <Component {...pageProps} />
+            <WagmiConfig client={wagmiClient}>
+              <RainbowKitProvider chains={chains}>
+                <Component {...pageProps} />
+              </RainbowKitProvider>
+            </WagmiConfig>
           </MantineProvider>
         </ColorSchemeProvider>
       </RecoilRoot>
